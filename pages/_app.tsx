5451f788903f38ff7713b21cd94fbfdb1c736afc
troubleshootid <kumarpyrrhos@gmail.com>
2025-10-03 15:11:23 +0900

    chore: remove Edge Runtime config for Vercel deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -113,8 +113,3 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 }
 
 export default MyApp;
-
-// Configure all pages to use Edge Runtime for Cloudflare Pages
-export const config = {
-  runtime: 'experimental-edge',
-};
