f1a400a8fecf70e990a948beb169bf27bb611d91
lijieQAQ <lijie13419722045@126.com>
2025-11-04 00:13:36 +0800

    fix: remove config test console

--- configs/app/features/publicTagsSubmission.ts
+++ configs/app/features/publicTagsSubmission.ts
@@ -1,16 +1,12 @@
 import type { Feature } from './types';
 
-import apis from '../apis';
 import services from '../services';
 import addressMetadata from './addressMetadata';
 
 const title = 'Public tag submission';
 
 const config: Feature<{}> = (() => {
-  console.log('services.cloudflareTurnstile.siteKey', services.cloudflareTurnstile.siteKey);
-  console.log('addressMetadata.isEnabled', addressMetadata.isEnabled);
-  console.log('apis.admin', apis.admin);
-  if (services.cloudflareTurnstile.siteKey && addressMetadata.isEnabled && apis.admin) {
+  if (services.cloudflareTurnstile.siteKey && addressMetadata.isEnabled) {
     return Object.freeze({
       title,
       isEnabled: true,
